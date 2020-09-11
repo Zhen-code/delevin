99f1e1393849fddda309a37b42eccabf7c73bb5a
wujialiang <17620835317@163.com>
2020-09-11 17:38:19 +0800

    登录

--- pages/login/index.js
+++ pages/login/index.js
@@ -15,48 +15,55 @@ Page({
     let userInfo = '';
     if (e.detail.errMsg == "getPhoneNumber:ok") {
       let msg = e.detail;
-      wx.getUserInfo({
-        success(res) {
-          userInfo = res.userInfo
-          wx.login({
-            success: res => {
-              if (res.code) {
-                wx.request({
-                  url: domain + api.login.authorization,
-                  data: {
-                    "code": res.code,
-                    "headImgUri": userInfo.avatarUrl,
-                    "nickName": userInfo.nickName
-                  },
-                  method: "POST",
-                  success(res) {
-                    let data = res.data.data;
-                    let source = {
-                      "encryptedData": msg.encryptedData,
-                      "iv": msg.iv,
-                      "openId": data.openId,
-                      "sessionKey": data.sessionKey,
-                    }
-                    request.login(source).then((res) => {
-                      let token = res.token
-                      wx.removeStorageSync('token')
-                      wx.setStorageSync('token', token)
-                      wx.navigateTo({
-                        url: "/pages/index/index"
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                userInfo = res.userInfo
+                wx.login({
+                  success: res => {
+                    if (res.code) {
+                      wx.request({
+                        url: domain + api.login.authorization,
+                        data: {
+                          "code": res.code,
+                          "headImgUri": userInfo.avatarUrl,
+                          "nickName": userInfo.nickName
+                        },
+                        method: "POST",
+                        success(res) {
+                          let data = res.data.data;
+                          let source = {
+                            "encryptedData": msg.encryptedData,
+                            "iv": msg.iv,
+                            "openId": data.openId,
+                            "sessionKey": data.sessionKey,
+                          }
+                          request.login(source).then((res) => {
+                            let token = res.token
+                            wx.removeStorageSync('token')
+                            wx.setStorageSync('token', token)
+                            wx.navigateTo({
+                              url: "/pages/index/index"
+                            })
+                          }).catch((err) => {
+                            console.log(err)
+                          })
+                        },
+                        fail(err) {
+                          console.log(err)
+                        }
                       })
-                    }).catch((err) => {
-                      console.log(err)
-                    })
-                  },
-                  fail(err) {
-                    console.log(err)
+                    } else {
+                      console.log('登录失败！')
+                    }
                   }
                 })
-              } else {
-                console.log('登录失败！')
               }
-            }
-          })
+            })
+          }
         }
       })
     } else {
